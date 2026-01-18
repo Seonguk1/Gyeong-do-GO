1c43201ab9a2ae5f375ffc07ca587be4b26f1492
qwasqwe0916 <qwasqwe0916@gmail.com>
2026-01-18 13:37:15 +0900

    init

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,73 +1,211 @@
-import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Alert, Modal, Pressable, Text, TouchableOpacity, View } from "react-native";
-
+import { Button, StyleSheet, View } from "react-native";
+import CustomModal from "../src/components/CustomModal";
+import InputArea from "../src/components/InputArea";
 
 
 export default function Index() {
-  const router = useRouter();
-  const [isSetupVisible, setIsSetupVisible] = useState(false);
-  return (
+  const [visible, setVisible] = useState(false);
+  const [joinVisible,setJoinVisible] = useState(false);
+  return(
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
       }}
     >
-      <Modal
-          animationType="slide"
-          transparent={true}
-          visible={isSetupVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-            setIsSetupVisible(!isSetupVisible);
-          }}>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}>
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </Pressable>
-            </View>
-          </View>
-      </Modal>
-      <TouchableOpacity
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          width: "50%",
-          height: "10%",
-          backgroundColor: 'powderblue',
-          marginBottom: "5%"
-        }}
-        onPress={()=>{setIsModalVisible(true)}}
+      <Button
+        title="방 생성" 
+        onPress={() => {setVisible(true)}}
+      />
+      <CustomModal
+        visible={visible}
+        setVisible={setVisible}
       >
-        <Text>방 생성</Text>
-      </TouchableOpacity>
-      
-      <TouchableOpacity
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          width: "50%",
-          height: "10%",
-          backgroundColor: 'powderblue'
-        }}
-        onPress={()=>{router.navigate("/home")}}
+        <InputArea title="최대 인원" text="최대 인원"/>
+        <InputArea title="시간(분)" text="시간(분)"/>
+        <InputArea title="경찰 수" text="경찰 수"/>
+        <Button title="방 만들기"/>
+      </CustomModal>
+
+      <Button
+        title="방 참가" 
+        onPress={() => {setJoinVisible(true)}}
+      />
+      <CustomModal
+        visible={joinVisible}
+        setVisible={setJoinVisible}
       >
-        <Text>방 참가</Text>
-      </TouchableOpacity>
-
-
-
-      <View>
-
-      </View>
-
+        <InputArea title="방 코드" text="방 코드"/>
+        <InputArea title="닉네임" text="닉네임"/>
+        <Button title="참가"/>
+      </CustomModal>
     </View>
   );
-  
 }
+
+// export default function Index() {
+//   const router = useRouter();
+//   const [setVisible, setSetVisible] = useState(false);
+//   const [joinVisible, setJoinVisible] = useState(false);
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+
+
+//       <Modal
+//           transparent={true}
+//           visible={setVisible}
+//           onRequestClose={() => {
+//             Alert.alert('Modal has been closed.');
+//             setSetVisible(!setVisible);
+//           }}>
+//           <View style={styles.centeredView}>
+//             <View style={styles.setView}>
+//               <Text style={styles.setText}>Hello World!</Text>
+//               <Pressable
+//                 style={[styles.button, styles.buttonClose]}
+//                 onPress={() => setSetVisible(!setVisible)}>
+//                 <Text style={styles.textStyle}>Hide Modal</Text>
+//               </Pressable>
+//             </View>
+//           </View>
+//       </Modal>
+
+  
+
+//       <View style={styles.container}>
+//       <Button 
+//         title="방 생성" 
+//         onPress={() => setSetVisible(true)} 
+//       />
+      
+//       </View>
+      
+
+
+//       <Modal
+//           transparent={true}
+//           visible={joinVisible}
+//           onRequestClose={() => {
+//             Alert.alert('Modal has been closed.');
+//             setJoinVisible(!joinVisible);
+//           }}>
+//           <View style={styles.centeredView}>
+//             <View style={styles.joinView}>
+              
+              
+              
+        
+//               <Pressable
+//                 style={[styles.button, styles.buttonClose]}
+//                 onPress={() => setJoinVisible(!joinVisible)}>
+//                 <Text style={styles.textStyle}>Hide Modal</Text>
+//               </Pressable>
+              
+//             </View>
+//           </View>
+//       </Modal>
+//       <TouchableOpacity
+//         style={{
+//           justifyContent: "center",
+//           alignItems: "center",
+//           width: "50%",
+//           height: "10%",
+//           backgroundColor: 'powderblue'
+//         }}
+//         onPress={()=>{setJoinVisible(true)}}
+//       >
+//         <Text>방 참가</Text>
+//         </TouchableOpacity>
+
+        
+//         <TouchableOpacity 
+//           style={{
+//             justifyContent: "center",
+//             alignItems: "center",
+//             width: "50%",
+//             height: "10%",
+//             backgroundColor: 'powderblue'
+//           }}
+//           onPress={() => setJoinVisible(true)}>
+//         <Text>방 참가</Text>
+//         </TouchableOpacity>
+//         <JoinIn
+//         visible={joinVisible} 
+//         onClose={() => setIsJoinVisible(false)}
+//         />
+
+
+//       <View>
+
+//       </View>
+
+//     </View>
+//   );
+// }
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  setView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 30,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  joinView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
---
+++ frontend/src/components/JoinIn.js
@@ -0,0 +1,44 @@
+import { Children } from "react";
+import { Modal, StyleSheet, View } from "react-native";
+
+
+const JoinIn = ({ visible, onClose}) => {
+  return (
+    <Modal
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.modalContainer}>
+            {Children}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContainer: {
+    width: '85%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+    elevation: 5,
+  },
+  closeButton: {
+    marginTop: 20,
+    backgroundColor: 'powderblue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  buttonText: { color: 'black', fontWeight: 'bold' }
+});
+export default JoinIn;
