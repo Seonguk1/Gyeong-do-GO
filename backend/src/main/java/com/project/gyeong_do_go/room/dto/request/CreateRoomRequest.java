7f2ce618d00c67a0c0d51637307996fee6057e41
황성욱 <dnrdl1109@gmail.com>
2026-02-05 16:42:34 +0900

    final 안 붙인 버그 수정

--- backend/src/main/java/com/project/gyeong_do_go/room/dto/request/CreateRoomRequest.java
+++ backend/src/main/java/com/project/gyeong_do_go/room/dto/request/CreateRoomRequest.java
@@ -1,10 +1,12 @@
 package com.project.gyeong_do_go.room.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Size;
 
 public record CreateRoomRequest(
-        @NotBlank(message = "BLANK") String nickname,
+        @NotBlank(message = "BLANK")
+        @Size(min = 2, max = 10, message = "INVALID_LENGTH")
+        String nickname,
         @NotNull(message = "NULL") Double latitude,
         @NotNull(message = "NULL") Double longitude,
         @NotNull(message = "NULL") int mapRadius,
