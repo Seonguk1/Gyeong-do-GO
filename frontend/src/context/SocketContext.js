9dd846b685ca154880507e17f15731ddce2c458c
황성욱 <dnrdl1109@gmail.com>
2026-02-10 17:20:11 +0900

    postGis 연동

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -27,7 +27,7 @@ export const SocketProvider = ({ children }) => {
     console.log(`🔌 [Global Socket] 연결 시작: Room ${roomId}`);
 
     const client = new Client({
-      brokerURL: 'ws://10.50.100.57:8080/ws', // IP 확인 필수
+      brokerURL: 'ws://192.168.0.195:8080/ws', // IP 확인 필수
       forceBinaryWSFrames: true,
       appendMissingNULLonIncoming: true,
 
