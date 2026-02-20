23c5d1d821eced895355f9d263646211e371cb85
qwasqwe0916 <qwasqwe0916@gmail.com>
2026-02-20 15:00:38 +0900

    game start popup

--- frontend/src/hooks/useRoomSocket.js
+++ frontend/src/hooks/useRoomSocket.js
@@ -3,10 +3,14 @@ import { useEffect, useMemo } from "react";
 
 export default function useRoomSocket(roomId, playerId) {
 <<<<<<< HEAD
+<<<<<<< HEAD
   const { connect, connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess, startVisible} = useSocket();
 =======
   const { connect, connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess} = useSocket();
 >>>>>>> ede2ea40 (구출, 검거, 이탈 구현)
+=======
+  const { connect, connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess, startVisible} = useSocket();
+>>>>>>> de7f06e9 (game start popup)
 
   // 1. 진입 시 연결 요청 (이미 연결돼 있으면 무시됨)
   useEffect(() => {
@@ -30,8 +34,12 @@ export default function useRoomSocket(roomId, playerId) {
   }, [roomData, playerId]);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   return { connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess, startVisible, ...result };
 =======
   return { connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess, ...result };
 >>>>>>> ede2ea40 (구출, 검거, 이탈 구현)
+=======
+  return { connected, disconnect, roomData, timeLeft, prisonerNumber, catchTheif, isOutOfBounds, rescue, rescueSuccess, startVisible, ...result };
+>>>>>>> de7f06e9 (game start popup)
 }
